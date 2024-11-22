1f22483457c3fe282699cb30e50e146b5077d272
AndriKoloina <raherimananakoloina@gmail.com>
2024-11-22 10:53:55 +0100

    class matrices

--- modules/Matrice.py
+++ modules/Matrice.py
@@ -1,25 +1,21 @@
 import random
 
 class Matrices:
-    
     def current_matrix(self, n):
-        # Créer une liste de valeurs de 1 à (n * n) - 1, et ajouter une case vide (0)
         values = [i + 1 for i in range(n * n - 1)]
-        values.append(0) 
+        values.append(0)
        
         random.shuffle(values)
         
+        matrix = [[0] * n for _ in range(n)]
         
-        matrix = [[0] * n for _ in range(n)] 
-        
-        
-        index = 0  
+      
+        index = 0
         for i in range(n):
             for j in range(n):
                 matrix[i][j] = values[index]
                 index += 1
-        
-
+       
         for i in range(n):
             for j in range(n):
                 print("{:3}".format(matrix[i][j]), end=" ")  
@@ -28,16 +24,16 @@ class Matrices:
         return matrix
 
     def end_matrix(self, n):
-        
+     
         matrix = [[0] * n for _ in range(n)]
         values = [i + 1 for i in range(n * n - 1)]
-        values.append(0)  
+        values.append(0)
+        
         index = 0
         for i in range(n):
             for j in range(n):
                 matrix[i][j] = values[index]
                 index += 1
-        
         
         for i in range(n):
             for j in range(n):
@@ -46,12 +42,15 @@ class Matrices:
         
         return matrix
 
-        
 
-dimension = int(input("Choisir la dimension 3 ou 4 : "))
 
-matrices = Matrices()
-
-matrices.current_matrix(dimension)
-print("---------------------------")
-matrices.end_matrix(dimension)
+if __name__ == "__main__":
+    dimension = int(input("Choisir la dimension 3 ou 4 : "))
+    if dimension == 4 or dimension == 3:
+        matrices = Matrices()
+        print("Matrice aléatoire :")
+        matrices.current_matrix(dimension)
+        print("\nMatrice finale :")
+        matrices.end_matrix(dimension)
+    else : 
+        print("Dimension: 3 ou 4")
